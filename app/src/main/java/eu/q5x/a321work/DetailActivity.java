9dba262de5090c623dcc9a45b241cfb8ee7903ca
Marcel <marcel.gangwisch@googlemail.com>
2016-06-18 15:15:25 +0200

    dependencies for osmdroid + including map to DetailActivity

--- app/src/main/java/eu/q5x/a321work/DetailActivity.java
+++ app/src/main/java/eu/q5x/a321work/DetailActivity.java
@@ -1,20 +1,39 @@
 package eu.q5x.a321work;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import eu.q5x.a321work.Model.SubTask;
 import us.feras.mdv.MarkdownView;
 
 
 public class DetailActivity extends AppCompatActivity {
+    MapView mapView;
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
     public static final String SUBTASK_ID = "subtaskId";
+    public static final String CATEGORY = "category";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        checkPermissions();
 
         String id = getIntent().getStringExtra(SUBTASK_ID);
         SubTask subTask = WorkApp.getSubTask(id);
@@ -24,6 +43,37 @@ public class DetailActivity extends AppCompatActivity {
             if (markdownView != null) {
                 markdownView.loadMarkdown(subTask.description);
             }
+
+            mapView = (MapView) findViewById(R.id.mapview);
+            mapView.setTileSource(TileSourceFactory.MAPNIK);
+            mapView.setBuiltInZoomControls(true);
+            mapView.setMultiTouchControls(true);
+            IMapController mapController = mapView.getController();
+            mapController.setZoom(9);
+            GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+            mapController.setCenter(startPoint);
         }
+
+        /*LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        locationManager.getLastKnownLocation() */
+    }
+
+    private void checkPermissions() {
+        List<String> permissions = new ArrayList<>();
+        String message = "osmdroid permissions:";
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            message += "\nLocation to show user location.";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            message += "\nStorage access to store map tiles.";
+        }
+        if (!permissions.isEmpty()) {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            String[] params = permissions.toArray(new String[permissions.size()]);
+            requestPermissions(params, REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+        } // else: We already have permissions, so handle as normal
     }
 }
