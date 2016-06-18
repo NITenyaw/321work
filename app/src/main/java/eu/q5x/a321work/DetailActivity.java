fb8a02d8e0a184ffed9866a97fbb211a1289f2f1
meinkej <jerome.meinke@googlemail.com>
2016-06-18 13:57:16 +0200

    Added MarkdownView.

--- app/src/main/java/eu/q5x/a321work/DetailActivity.java
+++ app/src/main/java/eu/q5x/a321work/DetailActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import eu.q5x.a321work.Model.SubTask;
+import us.feras.mdv.MarkdownView;
 
 
 public class DetailActivity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class DetailActivity extends AppCompatActivity {
         String id = getIntent().getStringExtra(SUBTASK_ID);
         SubTask subTask = WorkApp.getSubTask(id);
 
-        
+        MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdownView);
+        if (markdownView != null) {
+            markdownView.loadMarkdown(subTask.description);
+        }
     }
 }
